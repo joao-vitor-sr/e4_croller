9d534b082183f2cefdc033b2963d861d6215710e
joao-vitor-sr <joaovitorsrs7@gmail.com>
2021-01-27 20:32:46 -0300

    a new package in composer the vlucas/phpdotenv package

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5bb2e9cfb0767d3fb9868bf150f76787
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5bb2e9cfb0767d3fb9868bf150f76787', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit5bb2e9cfb0767d3fb9868bf150f76787', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit5bb2e9cfb0767d3fb9868bf150f76787
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5bb2e9cfb0767d3fb9868bf150f76787::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5bb2e9cfb0767d3fb9868bf150f76787($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5bb2e9cfb0767d3fb9868bf150f76787($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
