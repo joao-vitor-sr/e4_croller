76d4f9f5f601bc9215af93de87e9d1deaef32a13
joao-vitor-sr <joaovitorsrs7@gmail.com>
2021-01-27 21:00:30 -0300

    now the connect is using the .env file for configuration

--- oop/Database/Connect.php
+++ oop/Database/Connect.php
@@ -2,20 +2,35 @@
 
 namespace Files\Database;
 
-abstract class Connect 
+include_once __DIR__ . '/../../vendor/autoload.php';
+
+// dotenv configuration
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+abstract class Connect
 {
 	public function __construct()
 	{
-		$this->setPdo(new \PDO("mysql:host={$this->host};dbname={$this->dbName}", $this->user, $this->password));
+		$this->host = $_ENV['HOST'];
+		$this->port = $_ENV['PORT'];
+		$this->userName = $_ENV['USER_NAME'];
+		$this->password = $_ENV['PASSWORD'];
+		$this->database = $_ENV['DATABASE'];
+		
+		$this->setPdo(new \PDO("mysql:host={$this->host};port={$this->port};dbname={$this->database}", $this->userName, $this->password));
 	}
 
-	protected $pdo;
-	private $host = 'localhost';
-	private $dbName = 'gestao';
-	private $user = 'root';
-	private $password = 'e4J53787:';
+	private $host;
+	private $port;
+	private $userName;
+	private $password;
+	private $database;
 
-	// Getters and setters
+
+	protected $pdo;
+
+	// Getters and setters
 	public function setPdo($pdo)
 	{
 		$this->pdo = $pdo;
@@ -26,4 +41,3 @@ abstract class Connect
 		return $this->pdo;
 	}
 }
-
