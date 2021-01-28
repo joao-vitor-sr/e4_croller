c7d8b559773301f1918943d69c98e845565735cf
joao-vitor-sr <joaovitorsrs7@gmail.com>
2021-01-27 21:43:28 -0300

    .gitngore now is working

--- src/main.php
+++ src/main.php
@@ -2,10 +2,14 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$valuesFiles = getopt("f:l:");
+
+$csvFile = $valuesFiles['f'];
+$logFile = $valuesFiles['l'];
+
 $csvUpdateObj = new Files\Columns\CsvUpdate;
-$csvUpdateObj->csvFile = __DIR__ . '/../files/FinalizadosBovino.csv';
-$csvUpdateObj->logFile = __DIR__ . '/../files/log.txt';
+$csvUpdateObj->csvFile = __DIR__ . "/{$csvFile}";
+$csvUpdateObj->logFile = __DIR__ . "/{$logFile}";
 $csvUpdateObj->getEanCsvFile();
 
 var_dump($csvUpdateObj);
-
